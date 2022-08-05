6382fc091bbd9198dc01e6acb477bedc204b6a77
tuann07 <tuann07@users.noreply.github.com>
2022-08-05 12:28:48 +0700

    Initial Commit

---
+++ HigherOrLower/HigherOrLowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HigherOrLowerApp.swift
+//  HigherOrLower
+//
+//  Created by Tuan Nguyen Anh on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HigherOrLowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
